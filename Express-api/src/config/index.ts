b04301780eea347d010ff93ca87b7002328f2537
HtunHtun <htunhtuna02@gmail.com>
2026-03-31 23:49:19 +0630

    Initial setup with Prisma and Neon adapter

--- Express-api/src/config/index.ts
+++ Express-api/src/config/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const port = {
-  port: process.env.PORT || 7000,
+  port: process.env.PORT || 7000,   // Env file ကနေ လှမ်းယူ ပြီး မရှိရင် 7000 ကို default အနေနဲ့ သတ်မှတ်ထားတာ
   nodeEnv: process.env.NODE_ENV || "development",
 };
 export default port;
